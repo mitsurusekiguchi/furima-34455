f663a1829b786ce3e0068f4068a8805567cbb31b
chuntarou <chuntarou@chun.com>
2021-02-24 16:43:58 +0900

    画面遷移の設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -36,7 +36,9 @@ class ItemsController < ApplicationController
 
   def destroy
     item = Item.find(params[:id])
-    item.destroy
+    if item.destroy
+      redirect_to root_path
+    end
   end
 
   private

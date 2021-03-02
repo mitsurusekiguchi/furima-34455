f8ebd0fc798251f3822975d96541c1f8e485a3b9
chuntarou <chuntarou@chun.com>
2021-03-02 20:43:05 +0900

    画像を複数枚表示の実装

--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -3,17 +3,29 @@ if (document.URL.match( /new/ ) || document.URL.match( /edit/ )) {
     const ImageList = document.getElementById('image-list');
     const createImageHTML = (blob) => {
       const imageElement = document.createElement('div');
+      imageElement.setAttribute('class', "image-element")
+      let imageElementNum = document.querySelectorAll('.image-element').length
       const blobimage = document.createElement('img');
       blobimage.setAttribute('src',blob);
+
+      const inputHTML = document.createElement('input')
+      inputHTML.setAttribute('id', `item-image_${imageElementNum}`)
+      inputHTML.setAttribute('name', 'item[images][]')
+      inputHTML.setAttribute('type', 'file')
       
-      imageElement.appendChild(blobimage);
-      ImageList.appendChild(imageElement);
-    };
-    document.getElementById('item-image').addEventListener('change', function(e){
-      const imageContent = document.querySelector('img');
-      if (imageContent){
-        imageContent.remove();
-      }
+      imageElement.appendChild(blobimage)
+      imageElement.appendChild(inputHTML)
+      ImageList.appendChild(imageElement)
+
+      inputHTML.addEventListener('change', (e) => {
+        file = e.target.files[0];
+        blob = window.URL.createObjectURL(file);
+
+        createImageHTML(blob)
+      })
+    }
+    document.getElementById('item-image').addEventListener('change', (e) => {
+      
       const file = e.target.files[0];
       const blob = window.URL.createObjectURL(file);
 

34ac786bbcdb63e6e4c87bc5257ddbbc6d740440
chuntarou <chuntarou@chun.com>
2021-03-03 18:58:19 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
+
+  resources :users, only: [:show, :update]
   resources :items do
     resources :orders, only: [:index, :create]
   end

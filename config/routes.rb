e86accc56dc66e04fdc51614309b7fc5004253ce
chuntarou <chuntarou@chun.com>
2021-03-05 12:33:55 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root to: "items#index"
 
   resources :users, only: [:show, :update]
+  resources :cards, only: [:new, :create]
   resources :items do
     resources :orders, only: [:index, :create]
   end
